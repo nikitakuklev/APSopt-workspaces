4a93ed71e89e009c84c6f0417f49bcf22d1a3aef
nikitakuklev <spontaneouscoder@gmail.com>
2023-11-09 14:43:49 -0600

    Token no worky

--- nsls-ii/scripts/update_custom_code.sh
+++ nsls-ii/scripts/update_custom_code.sh
@@ -3,7 +3,11 @@
 python -m pip install --upgrade --force-reinstall --no-build-isolation --no-index --no-deps git+https://github.com/nikitakuklev/pysdds
 python -m pip install --upgrade --force-reinstall --no-build-isolation --no-index --no-deps git+https://github.com/nikitakuklev/Xopt
 python -m pip install --upgrade --force-reinstall --no-build-isolation --no-index --no-deps git+https://github.com/nikitakuklev/pybeamtools
-# this is a token to private repo, read-only, ok to be public
-python -m pip install --upgrade --force-reinstall --no-build-isolation --no-index --no-deps git+https://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@github.com/nikitakuklev/apsopt
+# this is a fine grained token to private repo, read-only, ok to be public, need to obfuscate or github will revoke it
+var0="github_pat"
+var1="11AAIC3CI0jm4BkyYZRUmH"
+var2="4WPY5xhczeFQgRCBiLavA0FOfFbfFiULYP4aow55Y4"
+var3="BHG45RATNIAIxu3e3"
+python -m pip install --upgrade --force-reinstall --no-build-isolation --no-index --no-deps git+https://${var0}_${var1}_${var2}${var3}@github.com/nikitakuklev/apsopt
 #python -m pip install --upgrade --force-reinstall --no-build-isolation --no-index --no-deps -e git+https://github.com/nikitakuklev/pyiota
 #python -m pip install --upgrade --force-reinstall --no-build-isolation --no-index --no-deps -e git+https://github.com/nikitakuklev/ocelot
