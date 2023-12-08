c0954240412a8c8d1b55aedf271cdc3a4ce3934d
nikitakuklev <spontaneouscoder@gmail.com>
2023-12-08 15:50:18 -0600

    New token

--- nsls-ii/scripts/update_custom_code.sh
+++ nsls-ii/scripts/update_custom_code.sh
@@ -5,9 +5,9 @@ python -m pip install --upgrade --force-reinstall --no-build-isolation --no-inde
 python -m pip install --upgrade --force-reinstall --no-build-isolation --no-index --no-deps git+https://github.com/nikitakuklev/pybeamtools
 # this is a fine grained token to private repo, read-only, ok to be public, need to obfuscate or github will revoke it
 var0="github_pat"
-var1="11AAIC3CI0jm4BkyYZRUmH"
-var2="4WPY5xhczeFQgRCBiLavA0FOfFbfFiULYP4aow55Y4"
-var3="BHG45RATNIAIxu3e3"
+var1="11AAIC3CI0HfJuKU0XJlAp"
+var2="2zANejWX3e36rlOqOwFsWAqp4SOL2EETv7ugIR"
+var3="lliFTBGMPFNRMJyFO0GoO"
 python -m pip install --upgrade --force-reinstall --no-build-isolation --no-index --no-deps git+https://${var0}_${var1}_${var2}${var3}@github.com/nikitakuklev/apsopt
 #python -m pip install --upgrade --force-reinstall --no-build-isolation --no-index --no-deps -e git+https://github.com/nikitakuklev/pyiota
 #python -m pip install --upgrade --force-reinstall --no-build-isolation --no-index --no-deps -e git+https://github.com/nikitakuklev/ocelot
